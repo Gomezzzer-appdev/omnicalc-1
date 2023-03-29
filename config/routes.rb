e09edc5d13fb1a6899c6623886c931d4e54e754e
Jorge Gomez <gomezgeorge132@gmail.com>
2023-03-29 16:58:21 +0000

    Square form looking good

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
 
+  get("/square/new", { :controller => "application", :action => "blank_square_form" })
+
+  #Write your routes above this line
+  # ================================
+
 end
