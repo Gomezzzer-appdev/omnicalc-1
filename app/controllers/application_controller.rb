6b732dd2eda6558319418e4dd538fef8b42bb4d4
Jorge Gomez <gomezgeorge132@gmail.com>
2023-03-29 17:27:54 +0000

    making progress with calculate square

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,13 @@
 class ApplicationController < ActionController::Base
      def blank_square_form
 
+      @num = params.fetch("elephant")
+      
       render({ :template => "calculation_templates/square_form.html.erb"})
+     end
+
+     def calculate_square
+
+        render({ :template => "calculation_templates/square_results.html.erb"})
      end
 end
